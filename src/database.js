4e545382ed839373109cb1eb71b3ca8621bd5f30
zhangzhuobin <zhangzhuobin@baidu.com>
2017-08-29 18:11:33 +0800

    Typo fixed

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCh9xtlktB1SAIAsCPZOK_WlLsRbGzTR8Y",
-  authDomain: "hankana-a4c27.firebaseapp.com",
-  databaseURL: "https://hankana-a4c27.firebaseio.com",
-  projectId: "hankana-a4c27",
-  storageBucket: "hankana-a4c27.appspot.com",
-  messagingSenderId: "1014280140204"
+  apiKey: 'AIzaSyCh9xtlktB1SAIAsCPZOK_WlLsRbGzTR8Y',
+  authDomain: 'hankana-a4c27.firebaseapp.com',
+  databaseURL: 'https://hankana-a4c27.firebaseio.com',
+  projectId: 'hankana-a4c27',
+  storageBucket: 'hankana-a4c27.appspot.com',
+  messagingSenderId: '1014280140204'
 })
 
 export default app
--- src/entry.js
+++ src/entry.js
@@ -16,4 +16,3 @@ router.add({
 })
 
 router.start()
-
