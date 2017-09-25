7dd23b243005139c958cd0010b86067e68929e3d
Dafrok <o.o@mug.dog>
2017-09-25 10:28:20 +0800

    Remove firebase, use wild dog instead.

--- src/database.js
+++ src/database.js
@@ -1,12 +1,20 @@
-import firebase from 'firebase'
-const app = firebase.initializeApp({
-  apiKey: 'AIzaSyCh9xtlktB1SAIAsCPZOK_WlLsRbGzTR8Y',
-  authDomain: 'hankana-a4c27.firebaseapp.com',
-  databaseURL: 'https://hankana-a4c27.firebaseio.com',
-  projectId: 'hankana-a4c27',
-  storageBucket: 'hankana-a4c27.appspot.com',
-  messagingSenderId: '1014280140204'
+import wilddog from 'wilddog'
+
+const app = wilddog.initializeApp({
+  syncURL: 'https://wd5917967826edddvs.wilddogio.com'
 })
 
 export default app
-export const db = app.database()
+export const db = app.sync()
+// import firebase from 'firebase'
+// const app = wilddog.initializeApp({
+//   apiKey: 'AIzaSyCh9xtlktB1SAIAsCPZOK_WlLsRbGzTR8Y',
+//   authDomain: 'hankana-a4c27.firebaseapp.com',
+//   databaseURL: 'https://hankana-a4c27.firebaseio.com',
+//   projectId: 'hankana-a4c27',
+//   storageBucket: 'hankana-a4c27.appspot.com',
+//   messagingSenderId: '1014280140204'
+// })
+
+// export default app
+// export const db = app.database()
